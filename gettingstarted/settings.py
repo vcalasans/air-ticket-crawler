4e76dbd4476bc66a3b20e9bd1b16df1026cc6654
Vinícius Calasans <vinicius.calasans@hotmail.com>
2019-12-04 00:50:49 -0300

    Still going

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
-
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -117,3 +117,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
 django_heroku.settings(locals())
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,8 @@
+import re
+import json
 from django.shortcuts import render
 from django.http import JsonResponse
+from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
@@ -8,54 +11,47 @@ from .models import Greeting
 
 def index(request):
     date = pd.datetime.today() + pd.offsets.Week()
-    headers = {
+    formattedDate = date.strftime('%Y-%m-%d')
+    htmlResponseText = requests.get(f"https://www.decolar.com/shop/flights/search/oneway/RIO/SAO/{formattedDate}/1/0/0/NA/NA/NA/NA/?from=SB&di=1-0").text
+    soup = BeautifulSoup(htmlResponseText, 'html.parser')
+    head = soup.head.text
+    searchParamsPattern = re.compile(r"^searchQuery : ({.*?})\n,$", re.MULTILINE | re.DOTALL)
+    searchParamsText = searchParamsPattern.search(head).group(1)
+    searchParamsDict = json.loads(searchParamsText)
+    print(searchParamsDict)
+    specialHeadersPattern = re.compile(r"""headers : {"X-UOW": '(.*?)',""", re.DOTALL)
+    XUOWHeader = specialHeadersPattern.search(head).group(1)
+    specialHHeaderReplacementPattern = re.compile(r'if\(o\.h\)o\.h = o\.h\.replace\("(.*?)"', re.DOTALL)
+    specialHHeaderReplacementScript = soup.find("script", text=specialHHeaderReplacementPattern).text
+    print(specialHHeaderReplacementScript)
+    specialHHeaderReplacement = specialHHeaderReplacementPattern.search(specialHHeaderReplacementScript).group(1)
+    print("special H header replacement: " + specialHHeaderReplacement)
+    searchParamsDict['h'] = searchParamsDict['h'].replace(specialHHeaderReplacement, "")
+    # searchParamsDict['h'] = "44ce942fe9e06b835507a0a67900c1ba"
+    # XUOWHeader = "results-17-1575343741175"
+    print('xuow is' + XUOWHeader)
+    searchParams = list(searchParamsDict.items())
+    print(searchParams)
+
+    searchHeaders = {
         'Accept': 'application/json, text/javascript, */*; q=0.01',
         'Accept-Encoding': 'gzip, deflate, br',
         'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7,fr;q=0.6,de;q=0.5,es;q=0.4',
         'Cache-Control': 'no-cache',
         'Connection': 'keep-alive',
         'Host': 'www.decolar.com',
-        'Postman-Token': '17b0d348-1049-45d3-83e2-1ef879f706c5,258e5d2d-cc81-4aeb-97ba-59ecd46a3e1f',
         'Pragma': 'no-cache',
         'Sec-Fetch-Site': 'same-origin',
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36',
-        'X-RequestId': 'aSgTfvwgRQ',
         'X-Requested-With': 'XMLHttpRequest',
-        'X-UOW': 'results-03-1575259116296',
+        'X-UOW': XUOWHeader,
         'XDESP-REFERRER': 'https://www.decolar.com/',
         'cache-control': 'no-cache',
     }
-    params = (
-        ('adults', '1'),
-        ('children', '0'),
-        ('infants', '0'),
-        ('limit', '4'),
-        ('site', 'BR'),
-        ('channel', 'site'),
-        ('from', 'RIO'),
-        ('to', 'SAO'),
-        ('departureDate', date.strftime('%Y-%m-%d')),
-        ('groupBy', 'default'),
-        ('orderBy', 'total_price_ascending'),
-        ('viewMode', 'CLUSTER'),
-        ('language', 'pt_BR'),
-        ('airlineSummary', 'false'),
-        ('chargesDespegar', 'false'),
-        ('user', 'b18f68ef-9226-4fd6-8f68-ef92266fd6f8'),
-        ('h', 'b9684fdcf50f468ae9c40c481674b3b1'),
-        ('flow', 'SEARCH'),
-        ('di', '1-0'),
-        ('clientType', 'WEB'),
-        ('disambiguationApplied', 'false'),
-        ('newDisambiguationService', 'true'),
-        ('initialOrigins', 'RIO'),
-        ('initialDestinations', 'SAO'),
-        ('pageViewId', 'e68b5d87-e9e9-4ad3-b518-6069de46b303'),
-    )
     response = requests.get(
         'https://www.decolar.com/shop/flights-busquets/api/v1/web/search',
-        headers=headers,
-        params=params
+        headers=searchHeaders,
+        params=searchParams
     )
     print(response.json())
     return JsonResponse(response.json())
